e3dc55c98a6c2865ab57d9ea2894608699fa9212
Marcus <mdeuce@hotmail.com>
2019-11-20 14:34:16 -0500

    Updated secure params for new sign up for role.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,9 +18,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i(first_name last_name
-                                                         phone admin))
+                                                         phone role))
     devise_parameter_sanitizer.permit(:account_update, keys: %i(first_name
                                                                 last_name phone
-                                                                admin))
+                                                                role))
   end
 end
